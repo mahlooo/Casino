62cb007be8c969b265514194efda5fb1bec691a8
mahlooo <mahlooo@users.noreply.github.com>
2014-12-05 17:00:01 -0500

    Remove rigged

--- src/main/java/io/malone/bukkit/casino/api/Gambler.java
+++ src/main/java/io/malone/bukkit/casino/api/Gambler.java
@@ -6,12 +6,10 @@ public class Gambler {
 
     private Player bukkit;
     private boolean playing;
-    private boolean rigged;
 
     public Gambler(Player bukkit) {
         this.bukkit = bukkit;
         this.playing = false;
-        this.rigged = false;
     }
 
     public boolean isPlaying() {
@@ -20,14 +18,6 @@ public class Gambler {
 
     public void setPlaying(boolean playing) {
         this.playing = playing;
-    }
-
-    public boolean isRigged() {
-        return rigged;
-    }
-
-    public void setRigged(boolean rigged) {
-        this.rigged = rigged;
     }
 
     public boolean testPermission(String permission) {
